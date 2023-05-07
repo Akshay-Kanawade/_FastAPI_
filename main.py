8646b946222403373df957de10b86221e5e7319b
Akshay_Kanawade <kanawade.akshay9@gmail.com>
2023-05-07 15:18:39 +0530

    Created http post route

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 # Import FastAPI class
 from fastapi import FastAPI 
+from fastapi.params import Body 
 
 
 # Create instance of fast api class. 
 app = FastAPI()   
 
  
-#  Create path operation or route using http method
+#  Create path operation or route using http get method
 @app.get("/")
 async def root():
     """ Routes which show simple message."""
     return {"message":"Created first path operation using fast api"}
 
-@app.get("/show-message")
+@app.get("/show-post")
 async def show_message():
-    """ Routes which show simple message using /show ."""
-    return {"message":"Created /show-message path operation using fast api"}
+    """ Routes which show simple message using /show-post ."""
+    return {"message":"Created /show-post path operation using fast api"}
+
+
+# Create route using http post
+@app.post("/create-post")
+async def create_Post():
+    """ Create a post using http post.
+    """
+    return {"message": "Successfully created post. "}
+
+# get post data from body
+@app.post("/create-post-from-body")
+def Create_post_from_body(payload: dict = Body(...)):
+    """ Create a post using http post and return the data from body.
+    """
+    return {"data":payload,"message": "Successfully created post. "}
+
+
